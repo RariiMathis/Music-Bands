e612b5c6b47556d30998eb47c33331ea55aa572b
Sackett <skeesen8@gmail.com>
2023-09-26 11:27:00 -0500

    added new event listener form to add a band with image,description, band name and working like button

--- Index.js
+++ Index.js
@@ -1,3 +1,13 @@
+
+
+
+
+
+
+
+
+
+
 fetch("http://localhost:3000/bands")
     .then((resp) => resp.json())
     .then(data => {
@@ -40,14 +50,50 @@ fetch("http://localhost:3000/bands")
     const form = document.getElementById('newBandForm')
     form.addEventListener('submit', (event) =>{
        event.preventDefault() 
-       const name = event.target.name.value
-       const image = event.target.image.value
-       const description = event.target.description.value
+       const newName = event.target.name.value
+       const newImage = event.target.image.value
+       const newDescription = event.target.description.value
        const newBand = {
-        name, 
-        image, 
-        description
+        name: newName,
+        image: newImage,
+        description: newDescription
+
        }
-       band(newBand)
-       event.target.reset
+       const addH2 = document.createElement("h2")
+        //div I am appending band info to location
+        divBandInfo = document.getElementById("display")
+        //band names rendered from db json
+        addH2.innerText = newBand.name
+
+        //add image element for each band name
+        const addImage = document.createElement("img")
+        //image rendered from db json
+        addImage.src = newBand.image
+        //Haylee
+        addImage.alt = newBand.name
+
+        const addDescription = document.createElement("h3")
+        addDescription.innerText = newBand.description
+
+        const likeButton = document.createElement("button")
+        let numberOfLikes = "0" 
+        likeButton.innerText = numberOfLikes + " Band Likes"
+        
+        likeButton.addEventListener("click", () =>{
+            console.log("click")
+            likeButton.innerText = ++numberOfLikes
+            + " Band Likes"
+            
+        })
+
+
+        //append elements
+        divBandInfo.append(addH2,addImage,addDescription,likeButton)
     })
+
+
+
+
+    //    band(newBand)
+    //    event.target.reset
+    
